6bef44ab214320cf424cea9db893aa7043314f08
Datisekai <84700751+datisekai@users.noreply.github.com>
2022-09-01 19:04:48 +0700

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,5 @@
-export const base_api = "https://dshoes-server.vercel.app/api";
+// export const base_api = "https://dshoes-server.vercel.app/api";
+export const base_api = "http://13.212.142.56:3000/api";
 export const base_auth = `${base_api}/auth`;
 export const base_products = `${base_api}/products`;
 export const base_comments = `${base_api}/comments`;
